0b1d2e106ac6183f97b4c6e3c60367fce14b6654
Branden Silva <branden@ambition.studio>
2026-04-03 09:49:40 -0600

    fix: correct sha256 values for all platforms

--- Formula/majortom.rb
+++ Formula/majortom.rb
@@ -3,26 +3,24 @@ class Majortom < Formula
   homepage "https://gomajortom.com"
   version "0.1.0"
   license "Apache-2.0"
-  # TODO: Update sha256 values after first release build.
-  # Run: shasum -a 256 majortom-*.tar.gz
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/gomajortom/groundcontrol/releases/download/v#{version}/majortom-darwin-arm64-v#{version}.tar.gz"
-      sha256 "6232ab23c82060529eb918bcd71af0bb1712553f40ef3108003e418fbb093b40"
+      sha256 "ae18057a71ae333b3a6490512c89ceb7db04a18b5b17120b103dac131d24f587"
     else
       url "https://github.com/gomajortom/groundcontrol/releases/download/v#{version}/majortom-darwin-x64-v#{version}.tar.gz"
-      sha256 "9d7f2be02f44db2acb67c5ca0d00d4be6752e94c588f0e7f7414e3cfc70fbdcc"
+      sha256 "6232ab23c82060529eb918bcd71af0bb1712553f40ef3108003e418fbb093b40"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/gomajortom/groundcontrol/releases/download/v#{version}/majortom-linux-arm64-v#{version}.tar.gz"
-      sha256 "b92bc0701a45c7bc6b1f160415f5b0ecc38c95dd506905b3863225b4f4f4153b"
+      sha256 "9d7f2be02f44db2acb67c5ca0d00d4be6752e94c588f0e7f7414e3cfc70fbdcc"
     else
       url "https://github.com/gomajortom/groundcontrol/releases/download/v#{version}/majortom-linux-x64-v#{version}.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "b92bc0701a45c7bc6b1f160415f5b0ecc38c95dd506905b3863225b4f4f4153b"
     end
   end
 
